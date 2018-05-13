5be77c4ff22bfaba57ad6b130af0f1624614d6ed
mkhuthir <mkhuthir@gmail.com>
2018-05-13 21:18:28 +0400

    progs: first push

---
+++ compile.sh
@@ -0,0 +1,3 @@
+g++ main.cpp -o main -std=c++11 -I/usr/include/python2.7 -lpython2.7
+g++ multi.cpp -o multi -std=c++11 -I/usr/include/python2.7 -lpython2.7
+g++ inv_sensor.cpp -o inv_sensor -std=c++11 -I/usr/include/python2.7 -lpython2.7
